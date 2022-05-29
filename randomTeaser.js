9694cad7046c2d3266a7cf3e43273372ec943fc9
Artem Belyaev <78081605+artem-belyaev@users.noreply.github.com>
2022-05-29 17:09:19 +0300

    update teaser

--- randomTeaser.js
+++ randomTeaser.js
@@ -4,14 +4,17 @@ var randomWords = ["муха", "лягушка", 'рыба', "выдра", "ду
 
 // Выбор случайной части тела из массива randomBodyParts:
 var randomBodyPart = randomBodyParts[Math.floor(Math.random() * 5)];
+var randomBodyPart2 = randomBodyParts[Math.floor(Math.random() * 5)];
 
 // Выбор случайного прилагательного из массива randomAdjectives:
 var randomAdjective = randomAdjectives[Math.floor(Math.random() * 4)];
+var randomAdjective2 = randomAdjectives[Math.floor(Math.random() * 4)];
 
 // Выбор случайного слова из массива randomWords:
 var randomWord = randomWords[Math.floor(Math.random() * 7)];
-
+var  randomWord2 = randomWords[Math.floor(Math.random() * 7)];
 // Соединяем случайные строки в предложение:
-var randomInsult = "У тебя " + randomBodyPart + " словно " + randomAdjective + " " + randomWord + "!!!";
+var randomInsult = "У тебя " + randomBodyPart + " словно " + randomAdjective + " " + randomWord + " и ещё " + randomBodyPart2 + " словно " + randomAdjective2 + " " + randomWord2
+  + " вот так !!!";
 
 randomInsult;
